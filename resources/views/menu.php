63bc6e35672865aea511bba43d85496133639c7a
stasya0903 <stasya0903@mail.ru>
2020-03-24 16:24:42 -0500

    added the routes for the about, welcome and news page

---
+++ resources/views/menu.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<nav class="navbar px-md-0 navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
+    <div class="container">
+        <div class="collapse navbar-collapse" id="ftco-nav">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item"><a href="/" class="nav-link">welcome page</a></li>
+                <li class="nav-item "><a href="/about" class="nav-link">About</a></li>
+                <li class="nav-item"><a href="/news" class="nav-link">News</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,12 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+Route::get('/about', function () {
+    return view('about');
+});
+Route::get('/news', function () {
+    return view('news');
 });
 
