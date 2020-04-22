bfa8e8e828bd6239a464d4899f72f5746accbf32
stasya0903 <stasya0903@mail.ru>
2020-04-21 19:48:15 -0500

    added table for resourses, added resourses id to news table

---
+++ database/migrations/2020_04_22_004411_alter_table_news_add_resource_id.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterTableNewsAddResourceId extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('news', function (Blueprint $table) {
+            $table->unsignedBigInteger('recourse_id')->default('1');
+            $table->foreign('recourse_id')
+                ->references('id')
+                ->on('resources')
+                ->onDelete('cascade');;
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('news', function (Blueprint $table) {
+            $table->dropforeign(['recourse_id']);
+            $table->dropColumn(['recourse_id']);
+
+        });
+    }
+}
