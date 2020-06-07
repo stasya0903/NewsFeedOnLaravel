f44c17f74286fd54c4dea48188a1181f94e203a5
stasya0903 <stasya0903@mail.ru>
2020-06-07 09:17:40 -0500

    rollback composer

--- app/Http/Controllers/SocialLoginController.php
+++ app/Http/Controllers/SocialLoginController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Adaptors\SocialMediaAdaptor;
-use App\Adaptors\SocialNetworkAuthService;
+use App\Services\SocialNetworkAuthService;
 use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
